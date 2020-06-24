48c12909b41fde8a008e433fddfe910c2840c0aa
Benedikt Gregor <benedikt@Benedikts-iMac.fritz.box>
2020-06-24 16:24:42 +0200

    cleanup

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('It Works!')
+var str = 'Hello'
+var world = ['W', 'o', 'r', 'l', 'd']
+
+var it1 = str[Symbol.iterator]()
+var it2 = world[Symbol.iterator]()
+
+console.log(it1.next())
+console.log(it1.next())
+console.log(it1.next())
+console.log(it1.next())
+console.log(it1.next())
+console.log(it1.next())
+console.log(it1.next())
+console.log(it1.next())
+
+console.log(it2.next())
+console.log(it2.next())
+console.log(it2.next())
